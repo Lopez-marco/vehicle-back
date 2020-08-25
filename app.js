bf57aca88548b214c3e5ca0a3f019720f4c22048
lopez-marco <lopez.marcohdez@gmail.com>
2020-08-24 21:26:54 -0400

    push to heroku 2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 let express = require("express");
 let app = express();
-
+// const fileUpload = require('express-fileupload');
 const sequelize = require("./db");
 
 let vehicle = require("./controllers/vehiclecontroller");
@@ -17,6 +17,25 @@ let user = require("./controllers/usercontrollers");
 
 sequelize.sync();
 app.use(require("./middleware/header"));
+// app.use(fileUpload());
+
+// //Upload 
+// app.post('/upload', (req, res) => {
+//     if (req.files === null) {
+//         return res.status(400).json({ msj: 'No file Upload' });
+//     }
+
+//     const file = req.files.file;
+
+//     file.mv(`${--dirname}/client/public/uploads/${file.name}`, err => {
+//         if (err) {
+//             console.error(err);
+//             return res.status(500).send(err);
+//         }
+
+//         res.json({ fileName: file.name, filePath: `/uploads/${file.name}` })
+//     })
+// })
 app.use(express.json());
 app.use("/user", user);
 ///////NO VALIDATE SESSION //////
