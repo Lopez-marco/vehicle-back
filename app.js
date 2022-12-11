e7cee7464f6b551c49b33c5a26319d3d131b1687
Marco Lopez <lopez.marcohdez@gmail.com>
2022-12-10 20:36:33 -0500

    Update packages

--- app.js
+++ app.js
@@ -8,14 +8,8 @@ let user = require("./controllers/usercontrollers");
 
 sequelize.sync();
 app.use(require("./middleware/header"));
-
 app.use(express.json());
 app.use("/user", user);
-///////NO VALIDATE SESSION //////
-///////NO VALIDATE SESSION //////
-///////NO VALIDATE SESSION //////if there is validate session on ports not need these
-///////NO VALIDATE SESSION //////
-///////NO VALIDATE SESSION //////
 app.use("/vehicle", vehicle);
 app.listen(process.env.PORT, function () {
     console.log("App is Listenig on port 3000");
